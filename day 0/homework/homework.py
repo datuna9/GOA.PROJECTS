43280d988c39aadf63091b8bbaef5d4fecb48b40
datuna9 <169091363+datuna9@users.noreply.github.com>
2024-05-06 21:54:50 +0200

    Create homework.py

---
+++ day 0/homework/homework.py
@@ -0,0 +1,72 @@
+from turtle import *
+#we want to paint a house 
+#step 1
+speed(15)
+width(6)
+color ("yellow")
+begin_fill()
+forward(200)
+left(90)
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+end_fill()
+#end of square
+
+#drawing a door
+forward(70)
+color("brown")
+begin_fill()
+left (90)
+forward(100)   #height of the door
+right(90)
+forward(60)
+right(90)
+forward(100)
+end_fill()
+
+penup()
+goto(200, 200)
+pendown()
+
+color("gray")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+#drawing window
+penup()
+goto(20,180)
+pendown()
+color("blue")
+begin_fill()
+left(30)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+end_fill()
+
+penup()
+goto(180,180)
+pendown()
+begin_fill()
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+end_fill()
+exitonclick()
